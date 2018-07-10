4873c560107db946e6dd6c3f9d3875365bbc851f
MRRaduan <math.raduan@gmail.com>
2018-07-10 13:54:30 -0300

    Finish ui jokes

--- app.js
+++ app.js
@@ -20,14 +20,26 @@ document.addEventListener("DOMContentLoaded", function() {
         if(response.type === 'success') {
           response.value.forEach(function(message) {
             output += `
-              <li>${message.joke}</li>
+              <li class="jokeItem">
+                <p class="jokeItem__text">
+                  "${message.joke}"
+                </p>
+               </li>
             `;
           });
         } else {
           output += `<li>Something went wrong..</li>`
         }
-
         document.querySelector('.jokes').innerHTML = output;
+        const modal      = document.querySelector('.modalBg');
+        const closeModal = document.querySelector('.modalClose');
+        modal.classList.add('active');
+
+        closeModal.addEventListener('click', function(){
+          modal.classList.remove('active');
+        });
+
+
       } 
     }
 
